ae000c7c0656db1c387cc107bc900e673d942d47
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-10-28 00:17:41 +0530

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import mylogo from "../assets/myLogo-Photoroom-Photoroom.jpg";
 import { Link } from "react-router-dom";
+import { SiLeetcode } from "react-icons/si";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaEnvelope,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
 
 function Footer() {
   return (
@@ -71,91 +79,63 @@ function Footer() {
           <hr className="my-6 w-full border-white border-t-richblack-500 sm:mx-auto lg:my-8" />
         </div>
         <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-white sm:text-center">
+          <span className="text-xl text-white sm:text-center">
             © 2024-
             <a href="https://github.com/Mimohshukla00" className=" text-pink">
               MimohShukla
             </a>
             . All Rights Reserved.
           </span>
-          <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
-            <Link to="#" className="text-white hover:text-teal">
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 8 19"
+          <ul>
+            <li className="flex flex-row space-x-8 mt-4 lg:mt-0">
+              <a
+                href="https://github.com/Mimohshukla00"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white hover:text-yellow-500"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <span className="sr-only">Facebook page</span>
-            </Link>
-            <a
-              href="https://discord.gg/jpRePQpC"
-              target="_blank"
-              className="text-white hover:text-teal"
-              rel="noopener noreferrer"
-            >
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 21 16"
+                <FaGithub size={25} />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/mimohshukla00"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white hover:text-yellow-500"
               >
-                <path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
-              </svg>
-              <span className="sr-only">Discord community</span>
-            </a>
-
-            <a
-              href="https://x.com/whomimohshukla"
-              target="_blank"
-              className="text-white hover:text-teal"
-              rel="noopener noreferrer"
-            >
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 17"
+                <FaLinkedin size={25} />
+              </a>
+              <a
+                href="https://leetcode.com/whomimohshukla"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white hover:text-yellow-500"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <span className="sr-only">Twitter page</span>
-            </a>
-            <a
-              href="https://github.com/Mimohshukla00"
-              target="_blank"
-              className="text-white hover:text-teal"
-              rel="noopener noreferrer"
-            >
-              <svg
-                className="w-4 h-4 hover:text-yellow-25"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
+                <SiLeetcode size={25} />
+              </a>
+              <a
+                href="https://twitter.com/@whomimohshukla"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white hover:text-yellow-500"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <span className="sr-only">GitHub account</span>
-            </a>
-          </div>
+                <FaTwitter size={25} />
+              </a>
+              <a
+                href="https://instagram.com/whomimohshukla"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white hover:text-yellow-500"
+              >
+                <FaInstagram size={25} />
+              </a>
+              <a
+                href="mailto:mimohshukla0001@gmail.com"
+                className="text-white hover:text-yellow-500"
+              >
+                <FaEnvelope size={25} />
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </footer>
