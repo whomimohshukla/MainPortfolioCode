cfd14bd08aef47a977eca2a8e8f01efebfd63550
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-10-28 00:18:16 +0530

    skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import skillsData from "../Data/Skills.js";
 import Slider from "react-slick";
+import { IoChevronForwardSharp } from "react-icons/io5";
+import { IoChevronBack } from "react-icons/io5";
 
 function Skills() {
-  var settings = {
+  const sliderRef = useRef(null);
+
+  const settings = {
+    arrows: false, // This removes the default Prev/Next arrows
     dots: true,
     infinite: false,
-    speed: 300,
-    // centerMode: true, // Ensures cards have space around them
+    speed: 400,
     slidesToShow: 3,
     slidesToScroll: 2,
-    initialSlide: 0,
-    nextArrow: <div className="slick-next custom-button">Next</div>,
-    prevArrow: <div className="slick-prev custom-button">Prev</div>,
+    initialSlide: 1,
     responsive: [
       {
         breakpoint: 1024,
@@ -44,22 +46,39 @@ function Skills() {
   };
 
   return (
-    <div className="skills-carousel  mt-20 ml-2 ">
-      <Slider {...settings}>
+    <div className="skills-carousel mt-12 mb-28 ml-0 relative">
+      {/* Custom Buttons */}
+      <div className=" ml-auto flex justify-center flex-row-reverse gap-3 mb-8">
+        <button
+          onClick={() => sliderRef.current.slickNext()}
+          className="  text-white px-5 py-5  border-transparent hover:border-yellow-500 border-2  transform transition-transform duration-200 cursor-pointer bg-skills hover:bg-opacity-45 rounded-full "
+        >
+          <IoChevronForwardSharp size={20}></IoChevronForwardSharp>
+        </button>
+        <button
+          onClick={() => sliderRef.current.slickPrev()}
+          className="  text-white px-5 py-5 bg-skills rounded-full hover:bg-opacity-60 border-transparent hover:border-yellow-500 border-2  transform transition-transform duration-200 cursor-pointer "
+        >
+          <IoChevronBack size={20}></IoChevronBack>
+        </button>
+      </div>
+
+      {/* Slider */}
+      <Slider ref={sliderRef} {...settings}>
         {skillsData.map((skill) => (
           <div
             key={skill.id}
-            className="skill-card text-white p-6 gap-x-36 rounded-lg shadow-lg h-60 mx-6"
+            className="skill-card bg-skills hover:bg-richblack-800 text-white p-6 gap-x-36 rounded-lg shadow-lg h-60 mx-6"
           >
             <img
               src={skill.icon}
               alt={`${skill.name} icon`}
-              className="h-32 w-34 mb-6  mt-7"
+              className="h-32 w-34 mb-6 mt-7"
             />
             <h3 className="mt-2 text-4xl font-bold text-richblue-5 ">
               {skill.name}
             </h3>
-            <p className=" text-xl mt-6 text-gray-400">{skill.description}</p>
+            <p className="text-xl mt-6 text-gray-400">{skill.description}</p>
           </div>
         ))}
       </Slider>
